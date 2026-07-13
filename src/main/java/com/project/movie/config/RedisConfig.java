f4345e88ff880021954981cbc363beaea1de34fd
PakinB <budphopakin@gmail.com>
2026-07-13 15:33:33 +0700

    fix: update RedisConfig to support password and SSL for Upstash

--- src/main/java/com/project/movie/config/RedisConfig.java
+++ src/main/java/com/project/movie/config/RedisConfig.java
@@ -4,20 +4,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedisConfig {
-	
-	@Value("${spring.redis.host}")
+
+	@Value("${spring.data.redis.host}")
 	private String host;
-	
-	@Value("${spring.redis.port}")
+
+	@Value("${spring.data.redis.port}")
 	private int port;
-	
+
+	@Value("${spring.data.redis.password:}")
+	private String password;
+
+	@Value("${spring.data.redis.ssl.enabled:false}")
+	private boolean sslEnabled;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+		if (!password.isEmpty()) {
+			config.setPassword(password);
+		}
+
+		LettuceClientConfiguration clientConfig = sslEnabled
+				? LettuceClientConfiguration.builder().useSsl().build()
+				: LettuceClientConfiguration.defaultConfiguration();
+
+		return new LettuceConnectionFactory(config, clientConfig);
 	}
-	
 }
